d21776d29231e179c8ad0d17e8ba6d29ab1792ca
wellynton.spagnol <wellyntonspagnol10@gmail.com>
2025-02-20 23:49:46 -0300

    adicionando texto feirafacil

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,25 @@
 import { Box } from "@/components/ui/box";
+import { Image } from "@/components/ui/image";
+import { Text } from "@/components/ui/text";
 import React from "react";
 import { ScrollView } from "react-native";
-import { Image } from "@/components/ui/image";
 
 export default function Home() {
   return (
     <Box className="flex-1 h-[100vh]" style={{ backgroundColor: "#1B755E" }}>
       <ScrollView
-        style={{ height: "100%" }}
+        className="h-full grow"
         contentContainerStyle={{ flexGrow: 1 }}
       >
         <Box className="flex flex-auto justify-center items-center">
           <Image
             size="2xl"
-            source={require('@/assets/images/logo.png')}
-            alt="image"
+            source={require("@/assets/images/logo.png")}
+            alt="Logo-FeiraFacil"
           />
+          <Text className="color-white font-bold text-center mt-4" size="4xl">
+            Feira Fácil
+          </Text>
         </Box>
       </ScrollView>
     </Box>
